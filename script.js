0e4924ea94f6b9405b271b613ad14b8ae090eff7
Xueyuan Su <xueyuan.su@gmail.com>
2020-11-29 14:13:39 -0800

    homepage v0.1 on github

---
+++ script.js
@@ -0,0 +1,35 @@
+/**
+
+Resume concept based on the Dribbble by John Wilson http://dribbble.com/shots/900308-Resume?list=users
+
+**/
+
+$(document).ready(function() {
+  $('#google1').animate({
+    borderBottomWidth: '15px'
+  }, 1000);
+  $('#oracle1').animate({
+    borderBottomWidth: '15px'
+  }, 1000);
+  $('#oracle2').animate({
+    borderBottomWidth: '150px'
+  }, 1000);
+  $('#upthere1').animate({
+    borderBottomWidth: '80px'
+  }, 1000);
+  $('#facebook1').animate({
+    borderBottomWidth: '200px'
+  }, 1000);
+  $('#growth1').animate({
+    borderBottomWidth: '65px'
+  }, 1000);
+  $('#integrity1').animate({
+    borderBottomWidth: '50px'
+  }, 1000);
+  $('#ads1').animate({
+    borderBottomWidth: '55px'
+  }, 1000);
+  $('#school2').animate({
+    borderBottomWidth: '87.5px'
+  }, 1000);
+})
